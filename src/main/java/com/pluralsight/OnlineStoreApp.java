68adf2bf92f0326519475b6d8cf6ed3cc01fcaf8
polinabreha <polinabreha@gmail.com>
2026-04-24 09:37:40 -0400

    Created ready to go product class

--- src/main/java/com/pluralsight/OnlineStoreApp.java
+++ src/main/java/com/pluralsight/OnlineStoreApp.java
@@ -3,7 +3,7 @@ package com.pluralsight;
 public class OnlineStoreApp {
     public static void main(String[] args) {
 
-        
+
 
     }
 
--- src/main/java/com/pluralsight/Product.java
+++ src/main/java/com/pluralsight/Product.java
@@ -1,4 +1,53 @@
 package com.pluralsight;
 
 public class Product {
+    private String SKU;
+    private String productName;
+    private double price;
+    private String department;
+
+    public Product(String SKU, String productName, double price, String department) {
+        this.SKU = SKU;
+        this.productName = productName;
+        this.price = price;
+        this.department = department;
+    }
+
+    public String getSKU() {
+        return SKU;
+    }
+
+    public void setSKU(String SKU) {
+        this.SKU = SKU;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String toString(){
+        return this.SKU + "|" + this.productName + "|" + this.price + "|" + this.department;
+    }
+
+
 }
